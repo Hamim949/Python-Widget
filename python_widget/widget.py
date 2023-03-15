b36c8101762ec3e1cd475ad85b7b0bd59cb82639
Mason Hamilton <Hamim949@hsd.k12.or.us>
2023-03-15 09:44:43 -0700

    updated search bar and results show up

--- python_widget/widget.py
+++ python_widget/widget.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QWidget, QLabel
+from PyQt6.QtWidgets import (QApplication, QVBoxLayout, QWidget, 
+                             QLabel, QHBoxLayout, QLineEdit, 
+                            QPushButton, QTextEdit)
 from PyQt6.QtGui import QIcon
 from PyQt6.QtCore import Qt
 import sys
@@ -14,10 +16,30 @@ class Window(QWidget):
     def UI(self):
         layout = QVBoxLayout()
         self.setLayout(layout)
- 
-        label = QLabel("Hello World")
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(label)
+
+    # Create the Widget # 
+        title_label = QLabel("Python Widget")
+        title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        description = "Search the Python Widget by Title,"
+        description += " Author, or Subject"
+        description_label = QLabel(description)
+
+        search_layout = QHBoxLayout()
+        self.search_field = QLineEdit()
+        search_button = QPushButton("Search")
+        search_layout.addWidget(self.search_field)
+        search_layout.addWidget(search_button)
+
+        results_text = QTextEdit("Results.")
+
+        layout.addWidget(title_label)
+        layout.addWidget(description_label)
+        layout.addLayout(search_layout)
+        layout.addWidget(results_text)
+
+
+
 def main(): 
     app = QApplication(sys.argv)
     window = Window()
