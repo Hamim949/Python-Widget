f972b48c302828dac394d19b16411ac61c37fdb1
Mason Hamilton <Hamim949@hsd.k12.or.us>
2023-03-17 08:37:47 -0700

    created controller.py

---
+++ python_widget/controller.py
@@ -0,0 +1,6 @@
+"""controller.py will manage making API calls to the gutendex and format the results for display"""
+
+# Import statements
+import requests
+
+# set some variables to help our API call
